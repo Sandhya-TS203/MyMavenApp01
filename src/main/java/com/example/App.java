1ce55db5999cd67438b03ec1668490044fa79f4f
Sandhya-TS203 <sandhyats72@gmail.com>
2026-05-11 09:39:45 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Kavya Nyamagoud,1BI23CS110" );
+    }
+}
